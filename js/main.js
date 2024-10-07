c4a6054935e4fb3ef6cee4e25a613d810685f808
wilfullmurder <mrfarrow2013@gmail.com>
2024-10-07 15:27:03 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,285 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Sticky header on scroll
+   */
+  const selectHeader = document.querySelector('#header');
+  if (selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToggle();
+    })
+  });
+
+  function mobileNavToggle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', () => { // Wrap scrollTo inside an arrow function
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  // Include GLightbox, Swiper, and Isotope libraries before using them
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  /**
+   * Init swiper slider with 3 slides at once in desktop view
+   */
+  new Swiper('.slides-3', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+  // Include Isotope library before using it
+
+  /**
+   * Porfolio isotope and filter
+   */
+  let portfolionIsotope = document.querySelector('.portfolio-isotope');
+
+  if (portfolionIsotope) {
+    let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+    let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+
+    window.addEventListener('load', () => {
+      let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+        itemSelector: '.portfolio-item',
+        layoutMode: portfolioLayout,
+        filter: portfolioFilter,
+        sortBy: portfolioSort
+      });
+
+      let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+      menuFilters.forEach(function(el) {
+        el.addEventListener('click', function() {
+          document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+          this.classList.add('filter-active');
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter')
+          });
+          if (typeof aos_init === 'function') {
+            aos_init();
+          }
+        }, false);
+      });
+
+    });
+
+  }
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 800,
+      easing: 'slide',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+
+// Function to set the active link
+function setActiveLink() {
+    // Get the current page name
+    let pageName = window.location.pathname.split('/').pop().split('.')[0];
+
+    // If the pageName is an empty string, set it to 'index'
+    if (pageName === '') {
+        pageName = 'index';
+    }
+
+    // If the pageName starts with 'blog-details', set it to 'blog'
+    if (pageName.startsWith('blog-details')) {
+        pageName = 'blog';
+    }
+
+    if(pageName.startsWith('technologies')){
+        pageName = 'technologies';
+    }
+
+    // Get all navbar links
+    const navbarLinks = document.querySelectorAll('#navbar nav ul li a');
+
+    // Loop through each link
+    navbarLinks.forEach(link => {
+        // Get the link's href attribute, remove the leading slash, the '..' and the .html extension
+        const linkHref = link.getAttribute('href').replace(new RegExp('^\\/'), '').replace('../', '').split('.')[0];
+
+        // If the link's href matches the current page name, add the 'active' class
+        if (linkHref === pageName) {
+            link.classList.add('active');
+        }
+    });
+}
+
+// Load the navbar
+fetch('/utilities/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        // Insert the navbar HTML into each page
+        document.getElementById('navbar').innerHTML = data;
+
+        // Call the function to set the active link
+        setActiveLink();
+
+        // Get the dropdown menu
+        var dropdownMenu = document.querySelector('.navbar .dropdown');
+
+        // Add a click event listener to the dropdown menu
+        dropdownMenu.addEventListener('click', function(event) {
+            // Check if the event target is a link
+            if (event.target.tagName !== 'A') {
+                // If the event target is not a link, prevent the default action
+                event.preventDefault();
+            }
+            // Toggle the 'dropdown-active' class on the dropdown menu
+            this.classList.toggle('dropdown-active');
+        });
+    })
+    .catch(error => console.error(error));
+
+
+    // Load the footer
+    fetch('/utilities/footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer').innerHTML = data)
+        .catch(error => console.error(error));
+
+    // Define a function to load a section
+    function loadSection(sectionId, sectionPath) {
+        // Get the element
+        const element = document.getElementById(sectionId);
+
+        // If the element exists, fetch the section HTML and insert it
+        if (element) {
+            fetch(sectionPath)
+                .then(response => response.text())
+                .then(data => element.innerHTML = data)
+                .catch(error => console.error(error));
+        }
+    }
+
+    // Use the function to load the sections
+    loadSection('call-to-action', '/utilities/call-to-action.html');
+    loadSection('team', '/utilities/team.html');
+    loadSection('why-us', '/utilities/why-choose-us.html');
+    loadSection('services-list', '/utilities/services-list.html');
+
+
+});
